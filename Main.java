18240be8031d6187587c7e6c1caaa40325cedca8
Fernando G. Luna <fer22luna@gmail.com>
2023-03-20 20:33:23 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+package primerProyecto;
+
+public class Main {
+	public static void main( String [] args) {
+        /*    
+     a)
+		int i = 1;
+		while(i<=100 ) {		
+			System.out.println(i);
+			i++;
+		}
+		*/
+	/*
+	 b)
+		for(int i=1;  i<=100;i++) {
+			System.out.println(i);
+		}
+		*/
+	 /*
+	 c)
+	  		int i = 1;
+		do  {
+			System.out.println(i);
+			i++;
+		}while(i<=100);
+		
+	 d)	
+	  */
+
+		for(int i=1;  i<=100;i++) {
+			System.out.println(i*2);
+		}
+		
+		
+	}
+}
